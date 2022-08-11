84835748ed1784cb35f745fa6edb2e3474a9b32f
ShafaetZamanShuvo <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2022-08-11 07:30:39 +0600

    adding student and teacher routes

--- backend/controllers/course-controllers.js
+++ backend/controllers/course-controllers.js
@@ -10,6 +10,7 @@ const CourseMaterials = require("../models/course_materials");
 const checkAuth = require("../middleware/check-auth");
 const express = require("express");
 const router = express.Router();
+const checkAuth = require("../middleware/check-auth");
 
 const getCoursesList = async(req, res, next) => {
     let courses;
--- backend/routes/admin-routes.js
+++ backend/routes/admin-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const adminController = require("../controllers/admin-controllers"); //this one is to import admin-controllers.js
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
@@ -10,10 +11,10 @@ router.get("/", adminController.getAdmin); // => localhost:5000/admin/.. if admi
 router.post("/login", adminController.adminLogin); // => localhost:5000/admin/login
 
 router.get("/courses", adminController.getCoursesList); // => localhost:5000/admin/courses  to show all the created courses
-
+router.use(checkAuth);
 router.post(
-  "/create-course/:sessionID",
-  adminController.adminCreateCourseForASession
+    "/create-course/:sessionID",
+    adminController.adminCreateCourseForASession
 ); // => localhost:5000/admin/create-course to create a new course
 
 router.delete("/delete/course/:courseID", adminController.adminDeleteCourse); // => localhost:5000/admin/delete-course to delete a course
@@ -28,8 +29,8 @@ router.post("/create/student", adminController.adminCreateStudent); // => localh
 router.get("/get/sessions", adminController.adminGetSessionList); // => localhost:5000/admin/get-sessions to get all the sessions
 
 router.get(
-  "/get/session/:sessionID",
-  adminController.adminGetSessionBySessionID
+    "/get/session/:sessionID",
+    adminController.adminGetSessionBySessionID
 ); // => localhost:5000/admin/get-session to get a session
 
 router.get("/get/users", adminController.getUsersList); // => localhost:5000/admin/create-user to create a new user')`
@@ -39,8 +40,8 @@ router.patch("/edit/user/:userID", adminController.adminEditUser); // => localho
 router.delete("/delete/user/:userID", adminController.adminDeleteUser); // => localhost:5000/admin/delete-user to delete a user
 
 router.patch(
-  "/removeUser/course/:courseID",
-  adminController.adminRemovesFromCourse
+    "/removeUser/course/:courseID",
+    adminController.adminRemovesFromCourse
 ); // => localhost:5000/admin/delete-all to delete user from course
 
 router.post("/create-session", adminController.adminCreateSession); // => localhost:5000/admin/create-session to create a new session
