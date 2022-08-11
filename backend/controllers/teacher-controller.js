5839a863594afc5d6fc92f3fb1d3ee83be7b2ee2
ShafaetZamanShuvo <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2022-08-11 12:25:45 +0600

    upload and delete files working properly in backend

--- backend/controllers/teacher-controller.js
+++ backend/controllers/teacher-controller.js
@@ -2,7 +2,6 @@ const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator"); //this one is to validate the inputs
 const mongoose = require("mongoose");
 const Course = require("../models/courses");
-
 const CourseMaterials = require("../models/course_materials");
 
 
@@ -77,9 +76,80 @@ const getCourseMaterials = async(req, res, next) => {
 };
 
 const deleteCourseMaterials = async(req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return next(
+            new HttpError("Invalid inputs passed, please check your data.", 422)
+        );
+    }
+
+    const { courseMaterialsId } = req.body;
     const courseId = req.params.courseID;
-    const courseMaterialsId = req.params.courseMaterialsID;
+
+    let course;
+    try {
+        course = await Course.findById(courseId);
+    } catch (err) {
+        const error = new HttpError(
+            "Something went wrong, could not find course.",
+            500
+        );
+        return next(error);
+    }
+
+    if (!course) {
+
+        const error = new HttpError(
+            "Could not find course for this course id.",
+            404
+        );
+        return next(error);
+    }
+
+    let courseMaterial;
+
+    try {
+        courseMaterial = await CourseMaterials.findById(courseMaterialsId).populate('course');
+        // await course.courseMaterials.pull(courseMaterial);
+    } catch (err) {
+        const error = new HttpError(
+            "Something went wrong, could not delete course materials.",
+            500
+        );
+        return next(error);
+    }
+
+    if (!courseMaterial) {
+        const error = new HttpError(
+            "Could not find course materials for this course id.",
+            404
+        );
+        return next(error);
+    }
+
+    try {
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await courseMaterial.remove({ session: session });
+        await course.courseMaterials.pull(courseMaterial);
+        await course.save({ session: session });
+        await session.commitTransaction();
+    } catch (err) {
+        const error = new HttpError(
+            "Something went wrong, could not delete course materials.",
+            500
+        );
+        return next(error);
+    }
+
+    res.status(200).json({ message: "Deleted course materials.", course: course });
+
+
+
+
+
 };
 
 exports.uploadCourseMaterials = uploadCourseMaterials;
 exports.getCourseMaterials = getCourseMaterials;
+exports.deleteCourseMaterials = deleteCourseMaterials;
--- backend/routes/teacher-routes.js
+++ backend/routes/teacher-routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const studentController = require('../controllers/student-controller');
-
+const teacherController = require('../controllers/teacher-controller');
+const { route } = require('./admin-routes');
+const uploadFile = require("../middleware/file-upload");
 const router = express.Router();
 
-
+router.get('/get-materials/:courseID', teacherController.getCourseMaterials); // => localhost:5000/teacher/course-materials/:courseID to get all the materials of a course
+router.post('/upload-material/:courseID', uploadFile.single("file"), teacherController.uploadCourseMaterials); // => localhost:5000/teacher/create-material/:courseID to create a new material
+router.delete('/delete-material/:courseID', teacherController.deleteCourseMaterials); // => localhost:5000/teacher/delete-material/:materialID to delete a material
 
 module.exports = router;
