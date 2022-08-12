a016209b18222c8f6c136406f20561ffe3752ab0
ShafaetZamanShuvo <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2022-08-12 14:45:05 +0600

    student routes

--- backend/controllers/student-controller.js
+++ backend/controllers/student-controller.js
@@ -4,10 +4,9 @@ const mongoose = require("mongoose");
 const Course = require("../models/courses");
 const CourseMaterials = require("../models/course_materials");
 const Assignment = require("../models/assignment");
-const Submissions = require("../models/submissions")
+const Submissions = require("../models/submissions");
 const User = require("../models/users");
 const Student = require("../models/students");
-
 
 const getAllCourses = async(req, res, next) => {
     let courses;
@@ -22,22 +21,17 @@ const getAllCourses = async(req, res, next) => {
     }
 
     if (!courses) {
-        const error = new HttpError(
-            "Could not find courses for this user.",
-            404
-        );
+        const error = new HttpError("Could not find courses for this user.", 404);
         return next(error);
     }
 
     res.json({ courses });
-
-}
+};
 
 const getEnrolledCourses = async(req, res, next) => {
     const user = req.userData.userId;
 
     if (!user) {
-
         const error = new HttpError("Could not find user for this user id.", 404);
         return next(error);
     }
@@ -54,20 +48,14 @@ const getEnrolledCourses = async(req, res, next) => {
     }
 
     if (!enrolledCourses) {
-        const error = new HttpError(
-            "Could not find courses for this user.",
-            404
-        );
+        const error = new HttpError("Could not find courses for this user.", 404);
         return next(error);
     }
 
     res.json({ courses: enrolledCourses.courses.toObject({ getters: true }) });
+};
 
-}
-
-
-
-const getCourseMaterials = async(req, res, next) => {
+const getCourseMaterials = async(req, res, next) => { //get all course materials for a course
     const courseId = req.params.courseID;
     let courseMaterials;
     try {
@@ -89,9 +77,9 @@ const getCourseMaterials = async(req, res, next) => {
     }
 
     res.json({ courseMaterials });
-}
+};
 
-const uploadSubmission = async(req, res, next) => {
+const uploadSubmission = async(req, res, next) => { //upload a submission for an assignment
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return next(
@@ -124,7 +112,6 @@ const uploadSubmission = async(req, res, next) => {
         return next(new HttpError("No file uploaded", 422));
     }
 
-
     const submission = new Submissions({
         file: req.file.path,
         assignment: assignmentId,
@@ -154,7 +141,7 @@ const uploadSubmission = async(req, res, next) => {
     res.status(201).json({ submission: submission });
 };
 
-const updateSubmission = async(req, res, next) => {
+const updateSubmission = async(req, res, next) => { //update a submission for an assignment
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return next(
@@ -202,7 +189,7 @@ const updateSubmission = async(req, res, next) => {
     res.status(200).json({ submission: submission });
 };
 
-const deleteSubmission = async(req, res, next) => {
+const deleteSubmission = async(req, res, next) => { //delete a submission for an assignment
     const error = validationResult(req);
     if (!error.isEmpty()) {
         return next(
@@ -214,7 +201,9 @@ const deleteSubmission = async(req, res, next) => {
 
     let submission;
     try {
-        submission = await Submissions.findById(submissionId).populate("assignment");
+        submission = await Submissions.findById(submissionId).populate(
+            "assignment"
+        );
     } catch (err) {
         const error = new HttpError(
             "Something went wrong, could not find submission.",
@@ -254,10 +243,9 @@ const deleteSubmission = async(req, res, next) => {
     }
 
     res.status(200).json({ message: "Deleted submission." });
-}
+};
 
-
-const getAllCourseAssignments = async(req, res, next) => {
+const getAllCourseAssignments = async(req, res, next) => { //get all assignments for a course
     const courseId = req.params.courseID;
 
     let courseAssignments;
@@ -282,7 +270,7 @@ const getAllCourseAssignments = async(req, res, next) => {
     res.json({ courseAssignments: courseAssignments });
 };
 
-const getCourseAssignmentByAssignmentD = async(req, res, next) => {
+const getCourseAssignmentByAssignmentD = async(req, res, next) => { //get all assignments for a course
     const assignmentId = req.params.assignmentID;
 
     let assignment;
@@ -307,14 +295,40 @@ const getCourseAssignmentByAssignmentD = async(req, res, next) => {
     res.json({ assignment: assignment });
 };
 
+const getSubmissionByAssignmentID = async(req, res, next) => { //get my submission for an assignment id
+    const assignmentId = req.params.assignmentID;
 
+    let submission;
+    try {
+        submission = await Submissions.findOne({
+            assignment: assignmentId,
+            user: req.userData.userId,
+        });
+    } catch (err) {
+        const error = new HttpError(
+            "Something went wrong, could not find submission.",
+            500
+        );
+        return next(error);
+    }
 
+    if (!submission) {
+        const error = new HttpError(
+            "Could not find submission for this submission id.",
+            404
+        );
+        return next(error);
+    }
 
+    res.json({ submission: submission });
+};
 
-
+exports.getEnrolledCourses = getEnrolledCourses;
+exports.getAllCourses = getAllCourses;
 exports.getCourseMaterials = getCourseMaterials;
 exports.getAllCourseAssignments = getAllCourseAssignments;
 exports.getCourseAssignmentByAssignmentD = getCourseAssignmentByAssignmentD;
 exports.uploadSubmission = uploadSubmission;
 exports.updateSubmission = updateSubmission;
 exports.deleteSubmission = deleteSubmission;
+exports.getSubmissionByAssignmentID = getSubmissionByAssignmentID;
--- backend/routes/student-routes.js
+++ backend/routes/student-routes.js
@@ -2,9 +2,30 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const studentController = require('../controllers/student-controller');
+const fileUpload = require("../middleware/file-upload");
+const checkAuthStudent = require("../middleware/check-auth-student");
 
 const router = express.Router();
 
+router.get('/get-all-courses', studentController.getAllCourses); // => localhost:5000/api/students/get-all-courses to get all the courses
+
+router.use(checkAuthStudent);
+
+router.get('/get-my-courses', studentController.getMyCourses); // => localhost:5000/api/students/get-my-courses to get all the courses of a student 
+
+router.get('/get-course-materials/:courseID', studentController.getCourseMaterials);
+
+router.get('/get-all-course-assignments/:courseID', studentController.getAllCourseAssignments);
+
+router.get('/get-course-assignment/:assignmentID', studentController.getCourseAssignmentByAssignmentD);
+
+router.post('/upload-submission/:assignmentID', fileUpload.single("file"), studentController.uploadSubmission);
+
+router.patch('/update-submission/:submissionID', fileUpload.single("file"), studentController.updateSubmission);
+
+router.delete('/delete-submission/:submissionID', studentController.deleteSubmission);
+
+router.get('get-submission/:assignmentID', studentController.getSubmissionByAssignmentID);
 
 
 module.exports = router;
